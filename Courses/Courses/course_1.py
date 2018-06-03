06a3dd11eadb0234527f677f2453196579b9a882
Waldo Rivier <waldo@citycable.ch>
2018-06-03 21:11:37 +0200

    .

--- Courses/Courses/course_1.py
+++ Courses/Courses/course_1.py
@@ -1,29 +1,31 @@
 
 import os
+from sqlalchemy.dialects.mssql.information_schema import columns
 from pathlib import PureWindowsPath
 import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
-import re as regexp
+import seaborn as sns
 
 try :
 
-    clientRootDir = PureWindowsPath('O:\\')
     workingDir = PureWindowsPath(os.getcwd())
+    dataDir = PureWindowsPath(workingDir.joinpath('Data'))
+    dataFile = PureWindowsPath(dataDir.joinpath('data.csv'))
+    df = pd.read_csv(dataFile.as_posix())
 
-    clientDir = PureWindowsPath(clientRootDir.joinpath('CAP','24 BenAdmin-Param','05 Paiement','SEPA'))
-    dataFileOrigine = PureWindowsPath(clientDir.joinpath('5_adresse_paiement_correction_is_iban.xlsx'))
-    dataFileCsvOrigine = PureWindowsPath(clientDir.joinpath('20_adresse_paiement_correction_Autres.csv'))
-    
-    ibanData = pd.read_excel(dataFileOrigine.as_posix())
+    df.info()
+    df.columns()
+    df.dtypes
 
-    ibanData.set_index(ibanData.NPERSO, inplace = True)
-    ibanData.STYPAIE = ibanData.STYPAIE.map({7:'IBAN/International'})
-
-    prog = regexp.compile('^CH')
-
+    numTypes = df.dtypes == (np.float64)
+    for i in range(numTypes.count()) :
+        # print (numTypes[i])
+        # print (df.columns[i])
+        if numTypes[i] :
+            df[df.columns[i]].sum()
 
 except ValueError as e  :
-    print ("ERROR")
+    print (e)
        
