babfc2adb594c2f9a16bfa179598655f3426e850
Waldo Rivier <waldo.rivier@aon.com>
2018-06-08 08:38:53 +0200

    .

--- Courses/Courses/course_1.py
+++ Courses/Courses/course_1.py
@@ -10,21 +10,24 @@ import seaborn as sns
 
 try :
 
-    workingDir = PureWindowsPath(os.getcwd())
-    dataDir = PureWindowsPath(workingDir.joinpath('Data'))
-    dataFile = PureWindowsPath(dataDir.joinpath('data.csv'))
+    working_dir = PureWindowsPath(os.getcwd())
+    data_dir = PureWindowsPath(working_dir.joinpath('Data'))
+    data_file = PureWindowsPath(data_dir.joinpath('data.csv'))
     df = pd.read_csv(dataFile.as_posix())
 
     df.info()
     df.columns()
     df.dtypes
 
+    df_numeric_columns = df.select_dtypes(include=['float64'])
+
     numTypes = df.dtypes == (np.float64)
     for i in range(numTypes.count()) :
-        # print (numTypes[i])
-        # print (df.columns[i])
-        if numTypes[i] :
-            df[df.columns[i]].sum()
+        print (numTypes[i])
+        print (df.columns[i])
+        if numTypes[i] == True :
+            sum = df[df.columns[i]].sum()
+            print (sum)
 
 except ValueError as e  :
     print (e)
