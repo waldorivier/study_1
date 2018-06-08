779b56ed51b5594e05d6bf24ecb44a49df2bd5c0
Waldo Rivier <waldo.rivier@aon.com>
2018-06-08 11:14:20 +0200

    .

--- Courses/Courses/course_1.py
+++ Courses/Courses/course_1.py
@@ -13,21 +13,34 @@ try :
     working_dir = PureWindowsPath(os.getcwd())
     data_dir = PureWindowsPath(working_dir.joinpath('Data'))
     data_file = PureWindowsPath(data_dir.joinpath('data.csv'))
-    df = pd.read_csv(dataFile.as_posix())
+    df = pd.read_csv(data_file.as_posix())
 
     df.info()
-    df.columns()
+    df.columns
     df.dtypes
 
-    df_numeric_columns = df.select_dtypes(include=['float64'])
+    df.count()
+    df.dropna(inplace=True)
+    df.count()
 
-    numTypes = df.dtypes == (np.float64)
-    for i in range(numTypes.count()) :
-        print (numTypes[i])
-        print (df.columns[i])
-        if numTypes[i] == True :
-            sum = df[df.columns[i]].sum()
-            print (sum)
+    # selection des colonnes de type numérique don on produit une copie
+    df_num_col = df.select_dtypes(include=['float64', 'int64']).copy()
+
+    # produit une série somme / max / min des valeurs prises par colonne
+    sum_num_col = df_num_col.sum()
+    max_num_col = df_num_col.max()   
+    min_num_col = df_num_col.min()   
+
+    # les 10 pays les plus "heureux"
+    df_hap_asc = df.sort_values(by = 'Happiness Rank', ascending = True)
+    df_hap_asc.head(10)
+
+    # les 10 pays les moins "malheureux"
+    df_hap_desc = df.sort_values(by = 'Happiness Rank', ascending = False)
+    df_hap_desc.head(10)
+
+
+
 
 except ValueError as e  :
     print (e)
