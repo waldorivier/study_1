804a9f2ea15f26195a7507238c0dfb29631d6c98
Waldo Rivier <waldo.rivier@aon.com>
2018-06-08 18:07:01 +0200

    .

--- Courses/Courses/course_1.py
+++ Courses/Courses/course_1.py
@@ -22,41 +22,64 @@ try :
     df.count()
     df.dropna(inplace=True)
     df.count()
-
-    
+        
     # selection des colonnes de type numérique don on produit une copie
     df_num_col = df.select_dtypes(include=['float64', 'int64']).copy()
 
     # produit une série somme / max / min des valeurs prises par colonne
     sum_num_col = df_num_col.sum()
-    max_num_col = df_num_col.max()   
-    min_num_col = df_num_col.min()   
 
-    # les 10 pays les plus "heureux"
+    print ('-----------------------------------')
+    print ('Max value from numerical columns')
+    print (df_num_col.max())   
+    print ('-----------------------------------')
+    
+    print ('-----------------------------------')
+    print ('Min value from numerical columns')
+    print (df_num_col.min())   
+    
     df_hap_asc = df.sort_values(by = 'Happiness Rank', ascending = True)
-    df_hap_asc.head(10)
+    print ('-----------------------------------')
+    print ('les 10 pays les + "heureux"')
+    print (df_hap_asc.head(10))
+    print ('-----------------------------------')
 
-    # les 10 pays les moins "malheureux"
+    print ('les 10 pays les - "heureux"')
     df_hap_desc = df.sort_values(by = 'Happiness Rank', ascending = False)
-    df_hap_desc.head(10)
+    print (df_hap_desc.head(10))
+    print ('-----------------------------------')
 
-    # suppression de la région Europe compte tenue que des sous-régions 
+    # suppression de la région Europe compte tenu que des sous-régions 
     # sont définies
     
     df_region = df.copy()
     df_region.set_index(['Region'], inplace=True)
     df_region.drop(['Europe'], axis=0, inplace=True)
 
-    #moyenne par région
-    ser_hap = df_region.groupby(['Region'])['Happiness Score'].mean()
-    ser_hap.sort_values(ascending=False)
-
-    #moyenne par région
-    f_above_6 = df_region['Happiness Score'] > 6
-    df_region_f = df_region[f_above_6]
+    # une variable qui définit le groupe by    
+    ser_region_group = df_region.groupby(['Region'])['Happiness Score']
+    
+    print ('moyenne des scores de bonheur par région')
+    ser_region_hap = ser_region_group.mean()
+    print (ser_region_hap.sort_values(ascending=False))
+    print ('-----------------------------------')
+    
+    print ('nombre de pays, par région, qui font un score > 6')
+    score_above_6 = df_region['Happiness Score'] > 6
+    df_region_f = df_region[score_above_6]
     ser_nb_region_f = df_region_f.groupby(['Region'])['Happiness Score']
-    ser_nb_region_f.count()
+    print (ser_nb_region_f.count())
+    print ('-----------------------------------')
    
+    print ('distance, par région, entre le score le max et le score min')
+    ser_dist_score = ser_region_group.max() - ser_region_group.min()
+    ser_dist_score.sort_values(ascending=False)
+    print(ser_dist_score)
+    print ('-----------------------------------')
+
+    print ('région avec la distance maximum entre le score le max et le score min')
+    print (ser_dist_score.head(1))
+    
 except ValueError as e  :
     print (e)
        
