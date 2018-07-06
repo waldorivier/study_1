e4094fcf82f607450293839b65183fb2d2490600
Waldo Rivier <waldo@citycable.ch>
2018-07-06 07:15:40 +0200

    .

--- Courses/Courses/others/exercices_module_2.py
+++ Courses/Courses/others/exercices_module_2.py
@@ -46,3 +46,20 @@ if 0:
 
 data_file = PureWindowsPath(data_dir.joinpath('tmdb_5000_movies.csv'))
 df_movies = pd.read_csv(data_file.as_posix())
+
+# nb valeurs manquantes sur toutes les colonnes et toutes les lignes
+df_movies.isnull().sum().sum()
+
+
+# supprimer seulement les lignes qui contiennent toutes des valeurs manquantes
+df_movies.isnull().dropna(how='all')
+
+# idem que ci-dessus mais pour les colonnes
+df_movies.dropna(how='all', axis=1)
+
+# supprime les colonnes (axis = 1) qui on moins plus 4000 lignes manquantes
+df_movies.dropna(thresh=4000, axis=1, inplace=True)
+
+# remplacement des données manquantes
+
+df_movies.overview.fillna(value = 'OVERVIEW_MISSING')
