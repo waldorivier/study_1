bb595d1adbf0feefd5749c5b5d6c98e554c801e9
Waldo Rivier <waldo@citycable.ch>
2018-08-24 20:57:51 +0200

    .

--- Courses/Courses/projects/course_2.py
+++ Courses/Courses/projects/course_2.py
@@ -1,15 +1,11 @@
 import os
-import pandas
-
 from pathlib import PureWindowsPath
-
 import numpy as np
 import pandas as pd
-
 import matplotlib.pyplot as plt
 import seaborn
-
 import sqlite3
+import utilities as util
 
 #-------------------------------------------------------------------------
 # répertoire de travail
@@ -30,7 +26,7 @@ def helper_store_df_to_db(df, db_name, table_name) :
     db_file = PureWindowsPath(data_dir.joinpath(db_name + ".db" ))
     db = sqlite3.connect(db_file.as_posix())
 
-    df_food_study_1.to_sql(table_name, db, chunksize=2000)
+    df.to_sql(table_name, db, chunksize=2000)
     db.close()
 
 def helper_load_df_from_db(db_name, table_name) :
@@ -53,8 +49,6 @@ def helper_store_csv_to_db(data_file, db_name, table_name) :
     for chunk in pd.read_csv(data_file, chunksize=2000):
         chunk.to_sql(name=table_name, con=db, if_exists="append", index=False)  
 
-
-
 #-------------------------------------------------------------------------
 # le fichier contenant les données brutes 
 # "en.openfoodfacts.org.products.tsv" est chargé dans une base 
@@ -62,6 +56,7 @@ def helper_store_csv_to_db(data_file, db_name, table_name) :
 #-------------------------------------------------------------------------
 
 def read_raw_data():
+
     data_file = PureWindowsPath(data_dir.joinpath('en.openfoodfacts.org.products.tsv'))
     helper_store_csv_to_db(data_file, "food", "data")
 
@@ -83,13 +78,12 @@ def export_null_columns(df, data_file) :
 #
 # Différents "thresh" sont appliqués aux données du df 
 #-------------------------------------------------------------------------
-
 def define_thresh_value (df) :
 
     threshes = np.arange(0, df.isnull().sum().max(), 10000)
 
-    # construction d'un DF à partir d'une liste de row construite 
-    # à l'aide d'un dictionnaire
+    # construction d'un df à partir d'une liste de lignes construites
+    # sur la base d'un dictionnaire
     rows = []
 
     def optimize_col_selection(thresh):
@@ -108,12 +102,10 @@ def define_thresh_value (df) :
     df_result = pd.DataFrame(rows)  
     df_result.set_index('schape_', inplace=True)
     
-    #-------------------------------------------------------------------------
     # le tableau ci-dessous indique le nombre de critères conservés et 
     # la moyenne des valeurs nulles des colonnes résiduelles conservées
     # par niveau (thresh) en fonction duquel une colonne est supprimée.   
-    #-------------------------------------------------------------------------
-    df_result
+    df_result.to_csv(data_dir.joinpath(data_result_name))
 
     df_result.plot(figsize=(16,12))
     plt.xlabel('nombre de colonnes')
@@ -124,62 +116,46 @@ def define_thresh_value (df) :
 #-------------------------------------------------------------------------
 # première étape de nettoyage des données
 # produit un df qui contient un premier ensemble de données épuréees
-# qui sont suavegardées en base de données 
+# qui sont sauvegardées en base de données 
 #-------------------------------------------------------------------------
-
-def clear_raw_data():
+def clean_raw_data():
 
     df_food = helper_load_df_from_db("food", "data")
 
-    #-------------------------------------------------------------------------
     # nettoyage des données
     # suppression de toutes les colonnes qui n'ont pas de données
     # réduction du tableau à 147 colonnes (initialement 163)
-    #-------------------------------------------------------------------------
-
     df_food.dropna(axis=1, how="all", inplace=True)
 
-    #-------------------------------------------------------------------------
     # gérer les doublons 
     # en l'occurence, pour les colonnes selectionnées, il n'y a pas de doublon
     # Cepedant, si l'on réduit à nouveau les critères, il est probable que 
     # des doublons apparaissent à nouveau (cf. plus loin)
-    #-------------------------------------------------------------------------
-    
     if df_food.drop_duplicates().shape == df_food.shape :
         print ("le tableau ne contient aucune ligne dupliquée")
+        
+    define_thresh_value(df_food)
 
-    #-------------------------------------------------------------------------
     # le niveau de 150'000 définit une sélection de 36 critères (comprenant 
     # tous les critères nécessaires à nos analyses)
-    #-------------------------------------------------------------------------
     df_food_cl = df_food.dropna(thresh=150000, axis=1)
     
-    #-------------------------------------------------------------------------
     # autorise l'affichage de toutes les colonnes
-    #-------------------------------------------------------------------------
     pd.set_option('display.max_columns', df_food_cl.shape[1]) 
     df_food_cl.head()
 
-    #-------------------------------------------------------------------------
     # suppression de toute les colonnes qui contiennent les mots "completed"
     # par exemple la colonne "states_tags" contient en grand nombre le texte "completed"
     # qui n'est pas relevant pour notre étude
-    #-------------------------------------------------------------------------
-    
     df_food_cl.states_tags.where(lambda x : x.str.contains("completed")).count()
 
     # suppression de toutes les colonnes dont le nom contient "states" et "tags" 
     # et qui contiennent "completed" en grand nombre
-
-    df_food_cl = df_food_sel.drop([col for col in df_food_cl.columns if "states" in col], axis=1)
-    df_food_cl = df_food_sel.drop([col for col in df_food_cl.columns if "tags" in col], axis=1)
+    df_food_cl = df_food_cl.drop([col for col in df_food_cl.columns if "states" in col], axis=1)
+    df_food_cl = df_food_cl.drop([col for col in df_food_cl.columns if "tags" in col], axis=1)
 
     # sauvegarde en base de données
-
     helper_store_df_to_db(df_food_cl, "df_food_cl", "df_food_cl")
-
-    return df_food_cl
 
 #-------------------------------------------------------------------------
 # ETUDE 1
@@ -187,37 +163,52 @@ def clear_raw_data():
 # repartition par macronutriment
 # conserver également la liste des ingredients
 # préparation de la base de données
-#
-# Le sous-ensemble de colonnes ci-dessous est conservé
 #-------------------------------------------------------------------------
+def setup_db_study_1():
 
+    df_food_study_1 = helper_load_df_from_db("df_food_cl", "df_food_cl")
 
-column_to_keep = set(['product_name', 'countries','ingredients_text','nutrition_grade_fr','energy_100g',
-                'fat_100g','proteins_100g', 'carbohydrates_100g'])
+    # le sous-ensemble comprenant les colonnes ci-dessous est conservé
+    column_to_keep = set(['product_name', 'created_datetime', 'last_modified_datetime', 
+                          'countries','ingredients_text','nutrition_grade_fr','energy_100g',
+                          'fat_100g','proteins_100g', 'carbohydrates_100g'])
 
-df_food_study_1 = df_food_sel.drop(axis=1, columns=set(df_food_sel.columns).difference(column_to_keep))
+    df_food_study_1 = df_food_study_1.drop(axis=1, 
+                                     columns=set(df_food_study_1.columns).difference(column_to_keep))
 
-# indication d'un index et tri sur celui-ci
+    # suppression de toutes les valeurs NAN
+    df_food_study_1.dropna(inplace=True)
+
+    # indication d'un index et tri sur celui-ci
+    df_food_study_1.set_index(['product_name'], inplace=True)
+    df_food_study_1.sort_index(inplace=True)
+
+    # export de l'index pour se rendre compte des doublons
+    df_food_study_1.index.to_series().to_csv(data_dir.joinpath(data_result_name))
+
+    # ceci permet d'extraire toutes les colonnes dont l'INDEX présente les doublons
+    df_food_study_1.loc[df_food_study_1.index.duplicated(),:]
+
+    # et inversément (sans doublons selon l'INDEX) à l'aide du ~ ..magique
+    df_food_study_1 = df_food_study_1.loc[~df_food_study_1.index.duplicated(),:]
+
+    # gestion des doublons 
+    df_food_study_1.drop_duplicates(inplace=True)
+
+    # gérer les valeurs extrêmes
+    # parmi toutes les colonnes numériques, supprimer les valeurs extrêmes correspondantes
+
+    for col_name in util.utilities.select_column_label(df_food_study_1, float):
+        df_food_study_1 = util.utilities.remove_outliers(df_food_study_1,col_name)
+    
+        print (df_food_study_1.shape) 
+
+    helper_store_df_to_db(df_food_study_1, "df_food_study_1", "df_food_study_1")
+
+df_food_study_1 = helper_load_df_from_db("df_food_study_1", "df_food_study_1")
 df_food_study_1.set_index(['product_name'], inplace=True)
 df_food_study_1.sort_index(inplace=True)
 
-#-------------------------------------------------------------------------
-# cela est insuffisant : en effet, au niveau de l'index, il reste des doublons
-#-------------------------------------------------------------------------    
-# export de l'index pour se rendre compte 
-df_food_study_1.index.to_series().to_csv(data_dir.joinpath(data_result_name))
-
-#-------------------------------------------------------------------------    
-# ceci permet d'extraire toutes les colonnes dont l'INDEX présente les doublons
-#-------------------------------------------------------------------------    
-df_food_study_1.loc[df_food_study_1.index.duplicated(),:]
-
-# et inversément (sans doublons selon l'INDEX) à l'aide du ~ ..magique
-df_food_study_1 = df_food_study_1.loc[~df_food_study_1.index.duplicated(),:]
-
-#-------------------------------------------------------------------------
-# gestion des doublons 
-#-------------------------------------------------------------------------
-df_food_study_1.drop_duplicates(inplace=True)
-
-helper_store_df_to_db(df_food_study_1, "df_food_study_1", "df_food_study_1")
+# parsing ingredients
+df_food_study_1.ingredients_text = df_food_study_1.ingredients_text.str.split()
+ser_ingredients = pd.Series(df_food_study_1.ingredients_text[0])
---
+++ Courses/Courses/projects/utilities.py
@@ -0,0 +1,42 @@
+
+import numpy as np
+import pandas as pd
+
+class utilities:
+    #-------------------------------------------------------------------------
+    # retourne la liste des noms de colonne d'un type donné
+    #-------------------------------------------------------------------------
+    def select_column_label(df, type):
+        columns = df.dtypes[df.dtypes.apply(lambda x : x == type)].index.tolist()
+        return columns
+
+    #-------------------------------------------------------------------------
+    # identifier et gérer les valeurs extrêmes 
+    # on considère le seuil de 1% pour déterminer les valeurs extrêmes; ce seuil
+    # determine (sous l'hypothèse de normalité de la distribution) toutes les valeurs 
+    # x | x > abs(mean - 3 * std) 
+    #-------------------------------------------------------------------------
+    
+    def remove_outliers(df, col_name):
+
+        def reject_(ser : pd.Series):
+            _mean = ser.mean() 
+            _std_1  = 3 * ser.std()
+  
+            def f_(x):
+                _reject = False
+
+                if np.abs(x - _mean) > _std_1 : 
+                    _reject = True
+                return _reject
+    
+            return f_
+
+        ser_col = df.loc[:,col_name]
+        f_reject = reject_(ser_col)
+        df = df.loc[~ser_col.apply(f_reject)]
+
+        return df
+ 
+   
+
