03ed1caf243ccb200222f9996a2e905ac8fc94f2
Waldo Rivier <waldo@citycable.ch>
2018-12-02 21:36:30 +0100

    warm-up T1

--- Courses/Courses/course_projects/course_3.py
+++ Courses/Courses/course_projects/course_3.py
@@ -14,8 +14,8 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error as mse
+from sklearn.metrics import mean_absolute_error as mae
 
-import seaborn as sns
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import scale
 from sklearn.model_selection import train_test_split
@@ -31,107 +31,88 @@ data_result_name = 'result.csv'
 pd.set_option('display.max_columns', 30)
 
 #-------------------------------------------------------------------------
-# Warm-up - TASK 1
+# Warm-UP - TASK 1
 #-------------------------------------------------------------------------
+def gradient(x, y, results, lr = None):
 
+    if lr is None:
+        lr = 0.8
 
+    a, b = 0, 0
+    n_steps = 400
 
-#-------------------------------------------------------------------------
-def evaluate_model_poly(degree, x, y, results,
-                        is_ridge = False, alpha_ridge = 0.04, plot = False):
-    reg = None
+    # Gradient descent
+    log_a = [a]
+    log_b = [b]
 
-    poly = PolynomialFeatures(degree, include_bias=False)
+    for step in range(n_steps):
+        # Compute partial derivatives
+        y_pred = a * np.log(x) + b
+        error = y - y_pred
+        a_grad = -2*np.mean(x*error)
+        b_grad = -2*np.mean(error)
 
-    poly.fit(x)
-    X = poly.fit_transform(x)
+        # Update parameters
+        a -= lr*a_grad
+        b -= lr*b_grad
 
-    X_tr, X_te, y_tr, y_te = train_test_split(
-        X, y, train_size=0.8, test_size=0.2, random_state=0)
+        # Log a, b values
 
-    if is_ridge :
-        if alpha_ridge is None:
-            reg = Ridge()
-        else:
-            reg = Ridge(alpha_ridge)
-    else :
-        reg = LinearRegression()
+        row = {}
 
-    reg.fit(X_tr, y_tr)
-        
-    y_tr_pred = reg.predict(X_tr)
-    y_te_pred = reg.predict(X_te)
+        row['a'] = a
+        row['b'] = b
 
-    # draw the model
-    x_model = np.linspace(min(x), max(x), num=100)
-    x_model = x_model[:, np.newaxis]
-
-    X_model = poly.transform(x_model)
-    y_model = reg.predict(X_model)
-    
-    row = {}
-    row['is_ridge'] = is_ridge
-    row['L2'] = np.sum(reg.coef_ ** 2)
-    row['degree'] = degree
-    row['alpha'] = alpha
-    row['mse_tr'] = mse(y_tr_pred, y_tr)
-    row['mse_te'] = mse(y_te_pred, y_te)
-    row['score_tr'] = reg.score(X_tr, y_tr)
-    row['score_te'] = reg.score(X_te, y_te)
-
-    if plot:
-        plt.plot(x_model, y_model)
-        plt.scatter(X_tr[:, 0], y_tr, label='train set')
-        plt.scatter(X_te[:, 0], y_te, label='test set')
-              
-    results.append(row)
+        results.append(row)
 
 #-------------------------------------------------------------------------
-
-data_file = data_dir.joinpath('bike-sharing-simple.csv')
+data_file = data_dir.joinpath('task-1.csv')
 data_df = pd.read_csv(data_file)
 
-x_ = data_df['temp'].values
-y = data_df['users'].values
-
-x_model = np.linspace(min(x_), max(x_), num=100)
-coefs = np.polyfit(x_, y, 10)
-y_model = np.polyval(coefs, x_model)
-plt.plot(x_model, y_model)
-y_pred = np.polyval(coefs, x)
-plt.scatter (x_, y)
-
-np.sqrt(mse(y, y_pred))
+x = data_df.iloc[:,0]
+y = data_df.iloc[:,1]
 
 #-------------------------------------------------------------------------
+# variable change x -> _x = log(x)
+_x = np.log(x)
 
-x = x_[:, np.newaxis]
+#-------------------------------------------------------------------------
+# fits a linear regression  
+coefs = np.polyfit(_x, y, deg = 1)
+y_pred = np.polyval(coefs, _x)
+
+#-------------------------------------------------------------------------
+# prints the model 
+x_model = np.linspace(min(_x), max(_x), num=100)
+y_model = np.polyval(coefs, x_model)
+
+plt.scatter(x, y, label='observations', color='blue')
+plt.plot(np.exp(x_model), y_model, label = 'model', color='red')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.legend()
+plt.show()
+
+#-------------------------------------------------------------------------
+# metrics 
+mse(y, y_pred)
+
+# compute baseline (default strategy = mean)
+_X = np.c_[_x]
+d_reg = DummyRegressor()
+d_reg.fit(_X, y)
+y_pred_bsl = d_reg.predict(_x)
+mse(y, y_pred_bsl)
+
+#-------------------------------------------------------------------------
+# Compares metrics with R2 scores 
+# total error - model error / total error => 1 - model error / total error
+
+R2 = 1 - mse(y, y_pred) / mse(y, y_pred_bsl)
+
+#-------------------------------------------------------------------------
 
 results = []
-for degree in np.arange(10, 11):
-    for alpha in np.logspace(-10, 0, num=100):
-        evaluate_model_poly(degree, x, y, results, True, alpha, False)
-        # evaluate_model_poly(degree, x, y, results, False, None, False)
-
-plt.legend()
-plt.show()
-
+gradient(x, y, results, 0.1)
 df_results = pd.DataFrame(results)
-df_results
 
-plt.semilogx(df_results.alpha, df_results.mse_tr, label='train curve')
-plt.semilogx(df_results.alpha, df_results.mse_te, label='test curve')
-plt.legend()
-plt.show()
-
-# determine the otpimum
-
-df_results
-df_results.iloc[df_results.mse_te.idxmin(),]
-
-evaluate_model_poly(10, x, y, results, True, 0.00475081, True)
-plt.legend()
-plt.show()
-
-#-------------------------------------------------------------------------
-    
