b266ed239432c63fff3a8c3ffaad04b8f24e3be0
Waldo Rivier <waldo@citycable.ch>
2018-12-05 14:21:03 +0100

    warm-up T2

--- Courses/Courses/course_projects/course_3.py
+++ Courses/Courses/course_projects/course_3.py
@@ -15,6 +15,9 @@ from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import mean_absolute_error as mae
+import itertools
+from scipy.linalg import lstsq
+from scipy import stats
 
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import scale
@@ -31,39 +34,48 @@ data_result_name = 'result.csv'
 pd.set_option('display.max_columns', 30)
 
 #-------------------------------------------------------------------------
+# perfoms lstsq on all features combinaisons
+#-------------------------------------------------------------------------
+
+results = []
+def compute_combination(data_df, target, features):
+
+    res = {'features' : [], 'w' : 0, 'rss' : 0, 'cn' : 0}
+
+    target_values = data_df[target].values
+    
+    data_df.drop([target], axis=1)
+    data_df = data_df[features]
+  
+    X = data_df.values
+    X1 = np.c_[np.ones(X.shape[0]), X]
+
+    w, rss, _, _ = lstsq(X1, target_values)
+    cn = np.linalg.cond(X1)
+
+    res['features'] = features
+    res['w'] = w
+    res['rss'] = rss
+    res['cn'] = cn
+
+    return res
+
+#combs = []
+#for i in np.arange(1, len(features)+1):
+#    els = [list(x) for x in itertools.combinations(features, i)]
+
+#    for el in els:
+#        res = compute_combination(data_df, target, el)
+#        results.append(res)
+
+# removes all rss = [] which signifies that rank is defficient
+
+# df_results = pd.DataFrame(results)
+# df_results = df_results[df_results.rss > 0]
+
+#-------------------------------------------------------------------------
 # Warm-UP - TASK 1
 #-------------------------------------------------------------------------
-def gradient(x, y, results, lr = None):
-
-    if lr is None:
-        lr = 0.8
-
-    a, b = 0, 0
-    n_steps = 400
-
-    # Gradient descent
-    log_a = [a]
-    log_b = [b]
-
-    for step in range(n_steps):
-        # Compute partial derivatives
-        y_pred = a * np.log(x) + b
-        error = y - y_pred
-        a_grad = -2*np.mean(x*error)
-        b_grad = -2*np.mean(error)
-
-        # Update parameters
-        a -= lr*a_grad
-        b -= lr*b_grad
-
-        # Log a, b values
-
-        row = {}
-
-        row['a'] = a
-        row['b'] = b
-
-        results.append(row)
 
 #-------------------------------------------------------------------------
 data_file = data_dir.joinpath('task-1.csv')
@@ -118,13 +130,91 @@ R2 = 1 - mse(y, y_pred) / mse(y, y_pred_bsl)
 #    finds a polynomial response (y) to problem with one feature (x)
 
 # Gradient descent : 
-#    general iterative algorithm solution
-#    apply to multi-features problem 
+#    general iterative algorithm 
+#    apply to multi-features problems
 
 
 #-------------------------------------------------------------------------
 
-results = []
-gradient(x, y, results, 0.1)
-df_results = pd.DataFrame(results)
+#-------------------------------------------------------------------------
+# Warm-UP - TASK 2
+#-------------------------------------------------------------------------
 
+data_file = data_dir.joinpath('task-2.csv')
+data_df = pd.read_csv(data_file)
+
+target = 'y'
+
+#-------------------------------------------------------------------------
+# give an idea of what we plot
+#-------------------------------------------------------------------------
+
+plt.scatter(data_df.x1, data_df[target], color = 'red')
+plt.scatter(data_df.x2, data_df[target], color = 'blue')
+plt.scatter(data_df.x3, data_df[target], color = 'green')
+plt.show()
+
+#-------------------------------------------------------------------------
+def evaluate_model(reg_type, data_df, featurs, target, results):
+
+    res = {}
+
+    X = data_df[features].values
+    y = data_df[target].values
+    y = np.c_[y]
+
+    X_tr, X_te, y_tr, y_te = train_test_split(
+        X, y, train_size=0.8, test_size=0.2, random_state=0)
+
+    # linear regression without removing outliers
+
+    if reg_type == 'linear':
+        reg = LinearRegression()
+    else:
+        if reg_type == 'huber':
+            reg = HuberRegressor(1.35)
+            y_tr = y_tr.flatten()
+    
+    reg.fit(X_tr, y_tr)
+
+    y_tr_pred = reg.predict(X_tr)
+    y_te_pred = reg.predict(X_te)
+
+    # metrics
+    
+    res['reg_type'] = reg_type
+    res['mae_tr'] = mae(y_tr_pred, y_tr)
+    res['mae_te'] = mae(y_te_pred, y_te)
+
+    results.append(res)
+
+    # finds out that (without shuffle) test scores better than train mse !
+    # because of the outliers, I guess
+
+#-------------------------------------------------------------------------
+
+features = data_df.columns.tolist()
+features.remove(target)
+
+results = []
+evaluate_model('linear', data_df, features, target, results)
+
+# removes outliers
+
+data_df_wo = data_df.copy()
+for f in data_df.columns:
+    z = (data_df_wo[f] - data_df_wo[f].mean()) / data_df_wo[f].std()
+    outliers = z >= 3
+
+    print (f, outliers.sum())
+    data_df_wo = data_df_wo[~outliers]
+
+evaluate_model('linear', data_df_wo, features, target, results)
+evaluate_model('huber', data_df, features, target, results)
+
+df_results = pd.DataFrame(results)
+df_results
+
+#-------------------------------------------------------------------------
+# Warm-UP - TASK 3
+#-------------------------------------------------------------------------
