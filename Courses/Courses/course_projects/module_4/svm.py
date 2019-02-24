bacece6364305677f1f9f2b3c7fbe00f134bf382
Waldo Rivier <waldo@citycable.ch>
2019-02-24 21:03:52 +0100

    .

--- Courses/Courses/course_projects/module_4/svm.py
+++ Courses/Courses/course_projects/module_4/svm.py
@@ -11,6 +11,9 @@ from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import mean_absolute_error as mae
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+import graphviz
 from sklearn.pipeline import Pipeline
 
 from sklearn.model_selection import train_test_split
@@ -19,6 +22,36 @@ import itertools
 import math
 import random as r  
 
-working_dir = os.getcwd()
-working_dir = os.path.join(working_dir,'course_projects', 'module_4')
-  
+#-------------------------------------------------------------------------------
+
+dir = os.getcwd()
+dir = os.path.join(dir,'course_projects', 'module_4')
+
+data_df = pd.read_csv(os.path.join(dir, 'titanic.csv'))
+
+#-------------------------------------------------------------------------------
+
+encoded_df = pd.get_dummies(data_df, columns=['pclass'])
+encoded_df.sex = [1 if (x=='male') else 0 for x in encoded_df.sex]
+
+features = encoded_df.drop(['name', 'survived'], axis=1)
+
+# Create X/y arrays
+X = features.values
+y = encoded_df.survived.values
+
+dt = DecisionTreeClassifier(
+    criterion='gini', max_depth=1, random_state=0)
+
+dt.fit(X, y)
+
+# Get score
+dt.score(X, y)
+
+dot_data = export_graphviz(
+    dt, out_file=None,
+    feature_names=features.columns, class_names=['died', 'survived'],
+    filled=True, rounded=True, proportion=True   
+)
+
+graphviz.Source(dot_data)
